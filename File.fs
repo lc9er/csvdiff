82069607808e40677632a599a2fa6d69fdc33c90
Brian McGraw <bjm25@drexel.edu>
2021-05-23 00:03:51 -0400

    throw error and exit if file not found

--- File.fs
+++ File.fs
@@ -2,9 +2,23 @@ namespace Csvdiff
 
 module ReadFile =
 
+    open System
     open System.IO
 
+    /// return the lines from the csv file
     let fetchLines filePath = // filename -> string array
 
-        let rows = File.ReadAllLines filePath
-        rows
+        if File.Exists filePath then
+            try 
+                File.ReadAllLines filePath
+            with
+            | :? FormatException as e ->
+                failwith e.Message
+            | :? IOException as e ->
+                failwith e.Message
+            | _ as e ->
+                failwith e.Message
+        else
+            printfn "File not found: %s" filePath
+            Environment.Exit 1
+            failwith filePath
