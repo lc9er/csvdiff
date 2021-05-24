223f77c8ab44c46c30582d2c32d37cdd78e54cd9
Brian McGraw <bjm25@drexel.edu>
2021-05-24 11:00:34 -0400

    updated file access

--- File.fs
+++ File.fs
@@ -2,23 +2,12 @@ namespace Csvdiff
 
 module ReadFile =
 
-    open System
     open System.IO
 
     /// return the lines from the csv file
     let fetchLines filePath = // filename -> string array
 
         if File.Exists filePath then
-            try 
-                File.ReadAllLines filePath
-            with
-            | :? FormatException as e ->
-                failwith e.Message
-            | :? IOException as e ->
-                failwith e.Message
-            | _ as e ->
-                failwith e.Message
+            File.ReadAllLines filePath
         else
-            printfn "File not found: %s" filePath
-            Environment.Exit 1
             failwith filePath
