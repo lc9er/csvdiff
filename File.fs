8eadd69901730a1384a539e7835e28d337498d8b
Brian McGraw <bjm25@drexel.edu>
2021-06-01 13:49:58 -0400

    fixed file path bug. Accidently hard-coded path with __SOURCE_DIRECTORY__

--- File.fs
+++ File.fs
@@ -9,7 +9,7 @@ module ReadFile =
     let fetchLines filePath separator =
 
         if File.Exists filePath then
-            let csv = CsvFile.Load( __SOURCE_DIRECTORY__ + filePath, (separator |> string ))
+            let csv = CsvFile.Load( Path.GetFullPath filePath, (separator |> string ))
             csv.Rows
         else
             failwith filePath
